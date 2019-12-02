fee8396d6d6c95c441fd9021efbfd32439e389a9
nephylum <25045705+nephylum@users.noreply.github.com>
2019-12-02 15:22:32 -0800

    add virtualenv with pipenv, adding Pipfile and Pipfile.lock

---
+++ lambdata_Eric_Ramon/__init__.py
@@ -0,0 +1,12 @@
+"""lambdata - a collection of data science helper functions"""
+
+import pandas as pd
+import numpy as np
+
+#sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeroes(10))
+
+#sample functions
+def increment(x):
+    return (x + 1)
---
+++ lambdata_Eric_Ramon/df_utils.py
@@ -0,0 +1,6 @@
+"""
+utility functions for working with DataFrames
+"""
+
+import pandas
+TEST_DF = pandas.DataFram([1,2,3,4,5,6])
