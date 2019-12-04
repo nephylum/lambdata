d82d57ee753b692fdbd77aa338b714129bce8131
nephylum <25045705+nephylum@users.noreply.github.com>
2019-12-04 13:00:28 -0800

    updated df_utils with 2 functions for code review

--- lambdata_Eric_Ramon/df_utils.py
+++ lambdata_Eric_Ramon/df_utils.py
@@ -3,4 +3,35 @@ utility functions for working with DataFrames
 """
 
 import pandas as pd
-TEST_DF = pd.DataFrame([1,2,3,4,5,6])
+from sklearn.model_selection import train_test_split
+
+TEST_DF = pd.DataFrame([1, 2, 3, 4, 5, 6])
+
+
+def train_val_test_split(df, target):
+    """
+    takes a dataframe and a target (column name), and returns
+    X_train, y_train, X_val, y_val, X_test, y_test (in that order)
+    """
+    X_tv, X_test, y_tv, y_test = train_test_split(df, df[target],
+                                                  test_size=0.15,
+                                                  random_state=42)
+
+    X_train, X_val, y_train, y_val = train_test_split(X_tv, y_tv,
+                                                      test_size=0.2,
+                                                      random_state=42)
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def null_check(df):
+    totalnull = 0
+    nulls = 0
+
+    for column in df:
+        nulls = df[column].isnull().sum()
+        if nulls > 0:
+            print('{} total nulls in {}'.format(nulls, column))
+            totalnull += nulls
+
+    return totalnull
--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
-    name="lambdata-DS9",
-    version = "0.1.1",
-    author = "alekslovesdata",
+    name="lambdata-DS9-Eric-Ramon",
+    version = "0.1.2",
+    author = "nephylum",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
